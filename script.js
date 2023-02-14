fc43c5919d77728f67bcb3ebc4f62f54115f6538
Foxlove <116038981+jadegfox@users.noreply.github.com>
2023-02-14 00:28:48 +0000

    add all the for loop

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let displayLink = document.querySelector(".display-link");
 
 
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
-let imageArts = ["https://m.media-amazon.com/images/M/MV5BN2ZkYzkzYmUtNzFjOC00YTRmLWIwYjEtMWZiYjI4Mzg4ZTJjXkEyXkFqcGdeQXVyNjk5NzY4OTk@._V1_.jpg","https://www.allkpop.com/upload/2022/06/content/161243/1655397793-en-dayet-50-2.jpg","https://i.scdn.co/image/ab67616d0000b273253a9c74941281b0407ce940","https://i.scdn.co/image/ab67616d0000b273c31e3f3a15f96cfc4c8f7b7a"];
+let imageArts = ["https://m.media-amazon.com/images/M/MV5BN2ZkYzkzYmUtNzFjOC00YTRmLWIwYjEtMWZiYjI4Mzg4ZTJjXkEyXkFqcGdeQXVyNjk5NzY4OTk@._V1_.jpg","https://mir-s3-cdn-cf.behance.net/projects/404/ed188b144786865.Y3JvcCwxMDgwLDg0NCwwLDI1MQ.jpg","https://ih1.redbubble.net/image.2537238537.6267/aps,504x498,medium,transparent-pad,600x600,f8f8f8.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH-0u2_BtHLmeFodM4OzN442knYIP50L8-YA&usqp=CAU"];
 let artists = ["BTS","enhypen","TXT","seventeen"]; 
 let songTitle = ["spring day","polaroid love","anti-romantic","world"]; 
 let songMusic = ["https://www.youtube.com/watch?v=OUutARJgw-g","https://www.youtube.com/watch?v=sdsPWMCKIkA","https://www.youtube.com/watch?v=d1w5eJozVjo"
@@ -48,7 +48,10 @@ let songMusic = ["https://www.youtube.com/watch?v=OUutARJgw-g","https://www.yout
 function addSongInfo() {
 
 // task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
-
+let newImage = image.vaule 
+let newSongName = song-name.vaule 
+let newArist = artist.vaule 
+let newSongLink = song-link.vaule 
 
 // task 10: use `.push()` to add each input value to the correct array.
 
@@ -69,7 +72,18 @@ function emptyDisplay() {
 
 
 function displaySongInfo() {
-
+imageArts.forEach(function(image){
+displayImage.insertAdjacentHTML("beforeend",`<img src=${image}/>`)
+})  
+artists.forEach(function(artist){
+displayArtist.insertAdjacentHTML("beforeend",`<p>${artist}</p>`)
+})  
+songTitle.forEach(function(Title){
+displaySong.insertAdjacentHTML("beforeend",`<p>${Title}</p>`) 
+})  
+ songMusic.forEach(function(Music){
+displayLink.insertAdjacentHTML("beforeend",`<p><a href=${Music}>link</a></p>`) 
+})   
 // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
 
 
